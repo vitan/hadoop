509ca96e56b8d4c1d43324a7e47182bebebec556
Weitao Zhou <wtzhou@dataman-inc.com>
2016-11-25 16:46:36 +0800

    fix

--- java7/2.7.3/yarn/docker-entrypoint.sh
+++ java7/2.7.3/yarn/docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+sed "s/HDFS_NAMENODE_RPC_HOST/$HDFS_NAMENODE_RPC_HOST/;s/HDFS_NAMENODE_RPC_PORT/$HDFS_NAMENODE_RPC_PORT/" ${HADOOP_INSTALL_DIR}/etc/hadoop/core-site.xml.template > ${HADOOP_INSTALL_DIR}/etc/hadoop/core-site.xml
+sed "s/JH_HOST/$JH_HOST/" ${HADOOP_INSTALL_DIR}/etc/hadoop/mapred-site.xml.template > ${HADOOP_INSTALL_DIR}/etc/hadoop/mapred-site.xml
+sed "s/RM_HOST/$RM_HOST/;s/RM_SCHEDULER/$RM_SCHEDULER/;s/HADOOP_TMP_DIR/$HADOOP_TMP_DIR/" ${HADOOP_INSTALL_DIR}/etc/hadoop/yarn-site.xml.template > ${HADOOP_INSTALL_DIR}/etc/hadoop/yarn-site.xml
+
 case "$ROLE" in
     resourcemanager|nodemanager|proxyserver):
         ${HADOOP_INSTALL_DIR}/bin/yarn --daemon start $ROLE
